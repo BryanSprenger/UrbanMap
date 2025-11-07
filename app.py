b9f288fba7b149d56e00887d3ea86c247ced2ebe
BryanSprenger <bsprenger.curitiba@gmail.com>
2025-11-07 20:49:27 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ st.set_page_config(page_title="Editor OSM Local", layout="wide")
 
 st.title("🗺️ Editor OSM - Versão Simples")
 
-with open("id_editor.html", "r", encoding="utf-8") as f:
+with open("UrbanMap/id_editor.html", "r", encoding="utf-8") as f:
     html_code = f.read()
 
 st.components.v1.html(html_code, height=700, scrolling=True)
